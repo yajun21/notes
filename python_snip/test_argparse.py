dd2da35f0c35ae2792698cb04212fa39d839cbe0
yajwu <wyjail@outlook.com>
2021-12-21 22:26:36 +0800

    2021.12.21: 生日快到了，等假期续命中。心疼交的税。

---
+++ python_snip/test_argparse.py
@@ -0,0 +1,18 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                    help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                    const=sum, default=max,
+                    help='sum the integers (default: find the max)')
+
+parser.add_argument('--foobar', action='store_true', default=False)
+parser.add_argument('--foo', dest='bar', type=int)
+
+args = parser.parse_args()
+print(args.integers)
+print(args.accumulate)
+print(args.accumulate(args.integers))
+print(args.foobar)
+print(args.bar, type(args.bar))
